95c2110d7d8419170359d40c853e5887050a0aa1
Alexander Matuzov <a.matuzov@gmail.com>
2017-10-27 00:36:13 +0300

    PR-5 WIP

--- src/main/java/com/beerhunter/prospector/entities/MinerEntity.java
+++ src/main/java/com/beerhunter/prospector/entities/MinerEntity.java
@@ -1,20 +1,138 @@
 package com.beerhunter.prospector.entities;
 
-import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+import net.minecraftforge.event.ForgeEventFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MinerEntity extends TileEntity implements IInventory {
 
     private ItemStack[] inventory;
     private String customName;
+    private int minePosition = -1;
+    private boolean mineIsComplete = false;
+    private short time = 1;
 
     public MinerEntity() {
+        super();
         this.inventory = new ItemStack[this.getSizeInventory()];
+    }
+
+    private void dig() {
+        if (time % 20 != 0) {
+            time++;
+            return;
+        } else {
+            time = 1;
+        }
+
+        if (mineIsComplete)
+            return;
+        if (inventory[0] == null || !(inventory[0].getItem() instanceof ItemPickaxe))
+            return;
+
+        int damage = inventory[0].getItemDamage();
+        int maxDamage = inventory[0].getMaxDamage();
+
+        if (damage + 1 > maxDamage)
+            return;
+
+        if (this.minePosition == -1)
+            this.minePosition = this.yCoord - 1;
+
+        Block block = worldObj.getBlock(xCoord, minePosition, yCoord);
+
+        if (this.minePosition == 0 || block.getBlockHardness(worldObj, xCoord, minePosition, yCoord) < 0)
+            return;
+
+        List<ItemStack> drops = getItemStackFromBlock(this.worldObj, this.xCoord, this.minePosition, this.zCoord);
+        this.worldObj.setBlockToAir(this.xCoord, this.minePosition, this.zCoord);
+        this.minePosition -= 1;
+        if (drops != null) {
+            inventory[0].setItemDamage(damage + 1);
+            for (ItemStack item : drops) {
+                mine(item);
+            }
+        }
+    }
+
+    public List<ItemStack> getItemStackFromBlock(World world, int x, int y, int z) {
+        Block block = world.getBlock(x, y, z);
+
+        if (block == null) {
+            return null;
+        }
+
+        if (block.isAir(world, x, y, z)) {
+            return null;
+        }
+
+        int meta = world.getBlockMetadata(x, y, z);
+
+        ArrayList<ItemStack> dropsList = block.getDrops(world, x, y, z, meta, 0);
+        float dropChance = ForgeEventFactory.fireBlockHarvesting(dropsList, world, block, x, y, z, meta, 0, 1.0F, false, null);
+
+        ArrayList<ItemStack> returnList = new ArrayList<ItemStack>();
+        for (ItemStack s : dropsList) {
+            if (world.rand.nextFloat() <= dropChance) {
+                returnList.add(s);
+            }
+        }
+
+        return returnList;
+    }
+
+    private void mine(ItemStack stack) {
+        stack.stackSize -= putToOutput(stack);
+
+        if (stack.stackSize > 0) {
+            float f = worldObj.rand.nextFloat() * 0.8F + 0.1F;
+            float f1 = worldObj.rand.nextFloat() * 0.8F + 0.1F;
+            float f2 = worldObj.rand.nextFloat() * 0.8F + 0.1F;
+
+            EntityItem entityitem = new EntityItem(worldObj, xCoord + f, yCoord + f1 + 0.5F, zCoord + f2, stack);
+
+            entityitem.delayBeforeCanPickup = 10;
+
+            float f3 = 0.05F;
+            entityitem.motionX = (float) worldObj.rand.nextGaussian() * f3;
+            entityitem.motionY = (float) worldObj.rand.nextGaussian() * f3 + 1.0F;
+            entityitem.motionZ = (float) worldObj.rand.nextGaussian() * f3;
+            worldObj.spawnEntityInWorld(entityitem);
+        }
+    }
+
+    private int putToOutput(ItemStack stack) {
+        for (int i = 10; i < 19; i++) {
+            if (stack.stackSize == 0)
+                return 0;
+            if (inventory[i] == null) {
+                inventory[i] = stack;
+                this.markDirty();
+                return stack.stackSize;
+            } else if (inventory[i].isItemEqual(stack)) {
+                int max = inventory[i].getMaxStackSize();
+                int free = max - inventory[i].stackSize;
+                if (free >= stack.stackSize) {
+                    inventory[i].stackSize += stack.stackSize;
+                    return stack.stackSize;
+                } else {
+                    inventory[i].stackSize = max;
+                    stack.stackSize -= free;
+                }
+            }
+        }
+        return stack.stackSize;
     }
 
     public String getCustomName() {
@@ -130,7 +248,8 @@ public class MinerEntity extends TileEntity implements IInventory {
 
     @Override
     public void updateEntity() {
-
+        if (!this.worldObj.isRemote)
+            dig();
     }
 
     @Override
@@ -151,12 +270,15 @@ public class MinerEntity extends TileEntity implements IInventory {
         if (this.hasCustomInventoryName()) {
             nbt.setString("CustomName", this.getCustomName());
         }
+
+        nbt.setInteger("minePosition", this.minePosition);
     }
 
 
     @Override
     public void readFromNBT(NBTTagCompound nbt) {
         super.readFromNBT(nbt);
+
 
         NBTTagList list = nbt.getTagList("Items", 10);
         for (int i = 0; i < list.tagCount(); ++i) {
@@ -168,6 +290,7 @@ public class MinerEntity extends TileEntity implements IInventory {
         if (nbt.hasKey("CustomName", 8)) {
             this.setCustomName(nbt.getString("CustomName"));
         }
-    }
 
+        this.minePosition = nbt.getInteger("minePosition");
+    }
 }
